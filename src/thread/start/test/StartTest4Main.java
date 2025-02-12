6fb4c894d36c5ffc7f38226f03bbdb9f89b66d5c
jndc-computer <wnstn9618@gmail.com>
2025-02-12 11:09:54 +0900

    2. 스레드 생성과 실행

---
+++ src/thread/start/test/StartTest4Main.java
@@ -0,0 +1,45 @@
+package thread.start.test;
+
+import static thread.util.MyLogger.log;
+
+public class StartTest4Main {
+
+    public static void main(String[] args) {
+        Thread threadA = new Thread(new MyThread("A", 1000), "Thread-A");
+        Thread threadB = new Thread(new MyThread("B", 500), "Thread-B");
+
+        threadA.start();
+        threadB.start();
+
+
+    }
+
+    static class MyThread implements Runnable {
+
+        private String threadName;
+        private int threadTime;
+
+        public MyThread(String threadName, int threadTime) {
+            this.threadName = threadName;
+            this.threadTime = threadTime;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                log(threadName);
+                try {
+                    Thread.sleep(threadTime);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+
+
+
+
+
+}
